99966c0fac6afefaa6b2ec66942be15ca4015f63
Filipe Tavares-Cadete <filipe@cadete.eu>
2020-10-21 21:19:28 +0000

    Operator description and first draft of code.

--- workspace.R
+++ workspace.R
@@ -1,15 +1,24 @@
 library(tercen)
 library(dplyr)
+library(scran)
+library(igraph)
 
-options("tercen.workflowId" = "wwww")
-options("tercen.stepId"     = "dddd")
+options("tercen.workflowId" = "f6d7883d26d906b1a8c4a3800d0616d4")
+options("tercen.stepId"     = "1d5b9f2e-760b-40a0-a53e-3d831a939da8")
 
 getOption("tercen.workflowId")
 getOption("tercen.stepId")
 
-(ctx = tercenCtx())  %>% 
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(median = median(.y)) %>%
-  ctx$addNamespace() %>%
-  ctx$save()
+ctx <- tercenCtx()
+
+pca_matrix <- ctx$as.matrix()
+
+snngraph <- buildSNNGraph(pca_matrix,
+                          k = k,
+                          type = edge_weighting)
+
+if (clustering_method == "Walktrap") {
+  clusters <- cluster_walktrap(snngraph)$membership
+} else if (clustering_method == "Louvain") {
+  clusters <- cluster_louvain(g)$membership
+}
